120c90384bc9b5febcad5b4ca3d3bdab4d8c500b
Kkabir Bhalla <kkabirbhalla@gmail.com>
2025-08-05 02:04:06 +0800

    Creates utils file and mehod to check dataloaded files

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import torchvision
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset,DataLoader
 from torchvision import transforms,datasets
+import numpy as np
+from utilFunctions import save_images
 
 #creating datasets and loader
-train_data = datasets.FashionMNIST(root = '/data',train=True,download=True,transform=transforms.ToTensor())
-test_data = datasets.FashionMNIST(root = '/data',train=False,download=True,transform=transforms.ToTensor())
+train_data = datasets.FashionMNIST(root = '/home/kkabir/Desktop/projects/fashionGAN/data',train=True,download=True,transform=transforms.ToTensor())
+test_data = datasets.FashionMNIST(root = '/home/kkabir/Desktop/projects/fashionGAN/data',train=False,download=True,transform=transforms.ToTensor())
 
 
 train_loader = DataLoader(train_data,batch_size=16,shuffle=True)
@@ -26,9 +28,16 @@ labels_map = {
     9: "Ankle Boot",
 }
 
-#hyperParams
+#hyperParams and device
 img_size = (28,28) #not needed since all images are the same size but added for future refrence if applied to other datasets
 num_epochs = 5
 lr = 0.01
 nc = 1 #number of channels, since grayscale only 1 channel
+nz = 100  #size of latent vector used to create the image
+ngf = 28
+ndf = 28
+lr = 0.0005
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+save_images(train_loader) #used to ensure dataloader working properly
 
---
+++ utilFunctions.py
@@ -0,0 +1,13 @@
+#used to store useful functions
+import numpy as np
+import matplotlib.pyplot as plt
+
+def save_images(loader):
+    real_batch = next(iter(loader))
+    image = real_batch[0][0]
+    np_image = image.numpy().squeeze()
+    plt.figure(figsize=(8,8))
+    plt.axis("off")
+    plt.title("Training Images")
+    plt.imshow(np_image,cmap = 'gray')
+    plt.savefig('test.png')

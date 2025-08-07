4874b267c6e5a0118b44978d3f68c5bfc8bbb6b1
Kkabir Bhalla <kkabirbhalla@gmail.com>
2025-08-07 23:49:55 +0800

    Started the generator class

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import torch
+import torch.nn as nn
 import torchvision
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset,DataLoader
 from torchvision import transforms,datasets
 import numpy as np
 from utilFunctions import save_images
+
+#
+set_seed = 100
+torch.manual_seed(set_seed)
+torch.use_deterministic_algorithms(True) #to allow for deterministic results
 
 #creating datasets and loader
 train_data = datasets.FashionMNIST(root = './data',train=True,download=True,transform=transforms.ToTensor())
@@ -41,3 +47,16 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 save_images(train_loader) #used to ensure dataloader working properly
 
+def initialize_weights(mo): #we are following the dcgan architecture so the weights should be normalized with mean = 0 and stdev = 0.02
+    cname = mo.__class__.__name__
+    if cname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif cname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+class Generator(nn.Module): #the generating image class
+    def __init__(self):
+        self.main = nn.Sequential(
+
+        )

6767892f2950eac234990984ce41eaf0117219ef
Kkabir Bhalla <kkabirbhalla@gmail.com>
2025-08-09 20:18:34 +0800

    Finished creating the generator class

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from torchvision import transforms,datasets
 import numpy as np
 from utilFunctions import save_images
 
+
 #
 set_seed = 100
 torch.manual_seed(set_seed)
@@ -40,8 +41,6 @@ num_epochs = 5
 lr = 0.01
 nc = 1 #number of channels, since grayscale only 1 channel
 nz = 100  #size of latent vector used to create the image
-ngf = 28
-ndf = 28
 lr = 0.0005
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -50,13 +49,35 @@ save_images(train_loader) #used to ensure dataloader working properly
 def initialize_weights(mo): #we are following the dcgan architecture so the weights should be normalized with mean = 0 and stdev = 0.02
     cname = mo.__class__.__name__
     if cname.find('Conv') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
+        nn.init.normal_(mo.weight.data, 0.0, 0.02)
     elif cname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
+        nn.init.normal_(mo.weight.data, 1.0, 0.02)
+        nn.init.constant_(mo.bias.data, 0)
 
 class Generator(nn.Module): #the generating image class
-    def __init__(self):
+    def __init__(self): #makes use of sigmoid function as all mnist data is between 0 and 1
+        super(Generator, self).__init__()
         self.main = nn.Sequential(
+            nn.ConvTranspose2d(nz,4,kernel_size=7,stride=1,dilation=1,bias=False) #leads to a 4 x 7 x 7 output
+            ,nn.BatchNorm2d(4)
+            ,nn.ReLU(True)
 
+            ,nn.ConvTranspose2d(4,2,kernel_size=2,stride=2,padding=0,bias=False) #leads to a 2 x 14 x 14 output
+            ,nn.BatchNorm2d(2)
+            ,nn.ReLU(True)
+
+            ,nn.ConvTranspose2d(2,1,kernel_size=2,stride=2,padding=0,bias=False) #leads toa 1 x 28 x 28 output
+            ,nn.Sigmoid()
         )
+    
+    def forward(self,input):
+        return self.main(input)
+
+netG = Generator().to(device)
+netG.apply(initialize_weights)
+print(netG)
+
+ahh = torch.randn(1,100,1,1,device=device)
+print(ahh.shape)
+res = netG(ahh)
+print(res.shape)
